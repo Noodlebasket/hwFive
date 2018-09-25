ed2f837c1305022b0d0012a29bcfb5f876692de4
Noodlebasket <41538280+Noodlebasket@users.noreply.github.com>
2018-09-25 14:16:51 -0400

    very basic html and some javascript

---
+++ assets/javascript/code.js
@@ -0,0 +1,44 @@
+$("#start").on('click', function() {
+    console.log("Clicked Start");
+    // for(var i=0; i<questions.length; i++) {
+    //     $('#subWrapper').append('<h2>'+questions[i].question+'</h2>');
+    //     for(var j=0; j<questions[i].answers.length;j++) {
+    //         $('#subWrapper').append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]);
+    //     }
+    // }
+});
+
+// var questions =[
+//     {
+//     question:"Where is London?",
+//     answers:["England", "Ireland", "Australia", "Greenland"],
+//     correctAnswer: "England"
+// }, {
+//     question:"Where is Bruges?",
+//     answers:["Belgium", "France", "Scotland", "Morocco"],
+//     correctAnswer: "Belgium"
+// }, {
+//     question:"Where is Warsaw?",
+//     answers:["Ukraine", "Poland", "Germany", "Russia"],
+//     correctAnswer: "Poland"
+// }, {
+//     question:"Where is Sydney?",
+//     answers:["New Zealand", "Morocco", "Cuba", "Australia"],
+//     correctAnswer: "Australia"
+// }, {
+//     question:"Where is Montreal?",
+//     answers:["France", "Canada", "Haiti", "America"],
+//     correctAnswer: "Canada"
+// }, {
+//     question:"Where is Jerusalem?",
+//     answers:["Saudi Arabia", "Egypt", "Pakistan", "Israel"],
+//     correctAnswer: "Israel"
+// }, {
+//     question:"Where is Hong Kong?",
+//     answers:["China", "Malaysia", "Japan", "India"],
+//     correctAnswer: "China"
+// }, {
+//     question:"Where is Rio De Jainero?",
+//     answers:["Argentina", "Brazil", "Nicaragua", "Chile"],
+//     correctAnswer: "Brazil"
+// }];
